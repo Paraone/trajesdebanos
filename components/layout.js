8ab4cf4e8e93566c5e67e4b60dd014d7cf94b723
Marcos Wade <marcoswade@gmail.com>
2022-11-06 15:22:00 -0500

    change meta data

--- components/layout.js
+++ components/layout.js
@@ -13,16 +13,14 @@ export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.jpg" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Trajes De Baños veniendo pronto!"
         />
         <meta
           property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content={`/images/logo.jpg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
